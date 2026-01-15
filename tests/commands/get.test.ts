baeddf0651794c751d022675c2ba10f810fc2aac
Dallas Crilley <dallas@dallascrilley.com>
2026-01-15 15:52:52 -0600

    fix(test): add findSimilarItems mock to buildGetCommand helper
    Prevents tests from hitting real 1Password CLI if itemExists=false

--- tests/commands/get.test.ts
+++ tests/commands/get.test.ts
@@ -14,6 +14,7 @@ function buildGetCommand() {
     checkOpCli: () => {},
     itemExists: () => itemExists,
     getItemFields: () => itemFields,
+    findSimilarItems: () => [],
     prompt: async () => ({ create: false }),
     applyColorConfig: () => {},
     createSpinner: () => ({
